2542b3274574b0c434aa713fe337c85528316be7
Markevich Daniil <dmarkevich@gmail.com>
2024-04-09 18:19:12 +0300

    add properties

--- API_Test/src/main/java/pages/BasePage.java
+++ API_Test/src/main/java/pages/BasePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import utilites.ConfProperties;
+
 public class BasePage {
-    static final String BASE_URL = "https://petstore.swagger.io/v2";
+    static final String BASE_URL = new ConfProperties().getProperty("base-url");
 }
